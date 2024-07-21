89dbddd9b80a1e170357f03b8c65e8604041aa13
Juliakuznetsovaaaa <99537178+Juliakuznetsovaaaa@users.noreply.github.com>
2024-07-21 16:22:03 +0300

    ttt

--- src/client/pages/About.tsx
+++ src/client/pages/About.tsx
@@ -12,7 +12,7 @@ export const About: React.FC = () => {
         <div className="col">
           <h1>About</h1>
           <p>
-            Author: Juli
+            Author: [Your name]
           </p>
         </div>
       </div>
